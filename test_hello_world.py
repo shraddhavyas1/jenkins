fed4ff2ea32471511b6227a12d6e7eaf0f11de2c
shraddhavyas1 <84119687+shraddhavyas1@users.noreply.github.com>
2024-05-14 23:47:04 +0530

    Create test_hello_world.py

---
+++ test_hello_world.py
@@ -0,0 +1,12 @@
+# test_hello_world.py
+
+import unittest
+from hello_world import hello_world
+
+class TestHelloWorld(unittest.TestCase):
+
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "Hello, World!")
+
+if __name__ == "__main__":
+    unittest.main()
